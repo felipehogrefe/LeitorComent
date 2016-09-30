af34302b2d3c81aab3d14f5317deee4933e0f705
felipehogrefe <felipehogrefe@gmail.com>
2016-09-30 16:58:04 -0300

    alguns bed smells de repeticao removidos

--- src/Caracteristica/Caracteristica.java
+++ src/Caracteristica/Caracteristica.java
@@ -15,82 +15,48 @@ public class Caracteristica {
 	private static final String[] video = {"VIDEO", "MUSICA", "MIDIA", "YOUTUBE"};									// 5
 	private static final String[] seguranca = {"SEGURA", "SEGURANCA", "PROTECAO", "SEGURO"};						// 6
 	private static final String[] velocidade = {"PESADO", "LENTO", "LEVE", "RAPIDO", "VELOCIDADE"};					// 7
-	private static final String[] update = {"ATUALIZACAO", "ATUALIZACOES", "UPDATE"};								// 8
-	private static final String[] memoria = {"MEMORIA", "RAM", "ESPACO"};											// 9
-	private static final String[] social = {"REDES", "SOCIAIS", "FACEBOOK", "TWITTER", "EMAIL", "E-MAIL"};			// 10
-	private static final ArrayList<String[]> listaCarac = new ArrayList<>();
+	private static final String[] memoria = {"MEMORIA", "RAM", "ESPACO"};											// 8
+	private static final String[] social = {"REDES", "SOCIAIS", "FACEBOOK", "TWITTER", "EMAIL", "E-MAIL"};			// 9
+	private static ArrayList<String[]> listaCarac = new ArrayList<>();
+	
+	private static void criaLista(){
+		if(listaCarac.size()==0){
+			listaCarac.add(complemento);
+			listaCarac.add(recurso);
+			listaCarac.add(bug);
+			listaCarac.add(visual);
+			listaCarac.add(download);
+			listaCarac.add(video);
+			listaCarac.add(seguranca);
+			listaCarac.add(velocidade);
+			listaCarac.add(memoria);
+			listaCarac.add(social);
+		}
+		
+	}
 				
 	public static void setaCaracteristicas(Browser b){
+		criaLista();	
+		
 		long nota;
+		int i;
 		String comentario;
 		ArrayList<Comentario> coment = b.lista;
 		ArrayCaracteristicas ac = b.contador;
+				
 		for(Comentario c : coment){
 			nota =c.getNota();
 			comentario = c.getComentario();
-//			System.out.println(nota);
-			for(String cc : complemento){
-//				System.out.println(cc);
-				if(comentario.contains(cc)){
-					ac.incrementaArray(0, nota);
-					break;
+			i=0;
+			for(String[] s : listaCarac){
+				for(String cc : s){
+					if(comentario.contains(cc)){
+						ac.incrementaArray(i, nota);
+						break;
+					}
 				}
+				i++;
 			}
-			for(String cc : recurso){
-				if(comentario.contains(cc)){
-					ac.incrementaArray(1, nota);
-					break;
-				}
-			}
-			for(String cc : bug){
-				if(comentario.contains(cc)){
-					ac.incrementaArray(2, nota);
-					break;
-				}
-			}
-			for(String cc : visual){
-				if(comentario.contains(cc)){
-					ac.incrementaArray(3, nota);
-					break;
-				}
-			}
-			for(String cc : download){
-				if(comentario.contains(cc)){
-					ac.incrementaArray(4, nota);
-					break;
-				}
-			}
-			for(String cc : video){
-				if(comentario.contains(cc)){
-					ac.incrementaArray(5, nota);
-					break;
-				}
-			}
-			for(String cc : seguranca){
-				if(comentario.contains(cc)){
-					ac.incrementaArray(6, nota);
-					break;
-				}
-			}
-			for(String cc : velocidade){
-				if(comentario.contains(cc)){
-					ac.incrementaArray(7, nota);
-					break;
-				}
-			}
-			for(String cc : memoria){
-				if(comentario.contains(cc)){
-					ac.incrementaArray(8, nota);
-					break;
-				}
-			}
-			for(String cc : social){
-				if(comentario.contains(cc)){
-					ac.incrementaArray(9, nota);
-					break;
-				}
-			}	
-		}		
+		}
 	}
-	
 }
